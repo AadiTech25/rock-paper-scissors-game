a715dd26596a7b96ff45969840627ac920503726
Adil <aadi.malik525@gmail.com>
2025-09-08 21:33:57 +0500

    added game files

---
+++ RPS.js
@@ -0,0 +1,77 @@
+let userScore = 0;
+let compScore = 0;
+
+const userScorePara = document.querySelector("#user-score");
+const compScorePara = document.querySelector("#comp-score");
+
+const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
+const resetBtn = document.querySelector("#reset-btn");
+
+const drawGame = () => {
+    msg.innerText = "Game was a Draw! Play Again.";
+    msg.style.backgroundColor = "#081b31";
+    msg.style.color = "white";
+};
+
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
+        userScore++;
+        userScorePara.innerText = userScore;
+        msg.innerText = `You Win! Your ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+        msg.style.color = "white";
+    } else {
+        compScore++;
+        compScorePara.innerText = compScore;
+        msg.innerText = `You Lose! ${compChoice} beats your ${userChoice}`;
+        msg.style.backgroundColor = "red";
+        msg.style.color = "white";
+    }
+};
+
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const playGame = (userChoice) => {
+    console.log("User Choice =", userChoice);
+    const compChoice = genCompChoice();
+    console.log("Computer Choice =", compChoice);
+
+    if (userChoice === compChoice) {
+        drawGame();
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            userWin = compChoice === "scissors" ? false : true;
+        } else {
+            userWin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userWin, userChoice, compChoice);
+    }
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
+
+const resetGame = () => {
+    userScore = 0;
+    compScore = 0;
+    userScorePara.innerText = userScore;
+    compScorePara.innerText = compScore;
+
+    msg.innerText = "Game Reset! Start Playing.";
+    msg.style.backgroundColor = "#081b31";
+    msg.style.color = "white";
+};
+
+resetBtn.addEventListener("click", resetGame);
